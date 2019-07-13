9ec2f0ee4083cbe92cb6de1cfd93d180bd07dd5d
clipqq <clifford.l.tran@gmail.com>
2019-07-13 12:08:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-function main(){
-function createShoppingList() {
+function main() {
+  function createShoppingList() {
     $('#js-shopping-list-form').on('submit', function(e) {
-        e.preventDefault()
-        console.log('button click')
+      e.preventDefault()
+      console.log('button click')
+      const userItem = $('#shopping-list-entry').val()
+      console.log(userItem)
+      $('.shopping-list').append(`
+        <li>
+          <span class="shopping-item">${userItem}</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>
+        `)
     })
+  }
+
+  function checkItem() {
+    $('.shopping-item-toggle').on('click', function(e) {
+      $(this).closest('.shopping-item').toggleClass('.shopping-item__checked')
+      console.log('check button click')
+    })
+  }
+
+
+  createShoppingList()
+  checkItem()
 }
-createShoppingList()
-}
+
 $(main)
